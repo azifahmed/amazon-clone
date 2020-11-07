f0bba093d483c6fcd12e2616290de0faf0840075
Asif Ahmed <azifahmed10@gmail.com>
2020-11-07 22:50:19 +0530

    Update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,6 +6,9 @@ function Checkout() {
         <div className="checkout">
             <div className="checkout__left">
                 <img className="checkout__ad" src="" alt="/" />
+                <div>
+                    h2.checkout__title
+                </div>
             </div>
             
         </div>
