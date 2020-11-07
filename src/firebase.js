f583415d1514fff275230b9ab3335efd5260139c
Asif Ahmed <azifahmed10@gmail.com>
2020-11-07 22:44:01 +0530

    amazon commit 1

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9dBt8VPiCZReUB2EwGoPXaFd5geY90gU",
+    authDomain: "clone-a42fc.firebaseapp.com",
+    databaseURL: "https://clone-a42fc.firebaseio.com",
+    projectId: "clone-a42fc",
+    storageBucket: "clone-a42fc.appspot.com",
+    messagingSenderId: "710724792216",
+    appId: "1:710724792216:web:b9286b7b658346c24705ed",
+    measurementId: "G-KD7CTGW4JV"
+  };
